ea1aed5f6994007fc878447019be7dbb83bcb2ab
Hondamir <157780018+Hondamiras@users.noreply.github.com>
2025-02-27 17:46:09 +0500

    Database

--- config/settings.py
+++ config/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# import dj_database_url
-
 # DATABASES = {
-#     'default': dj_database_url.config(default="sqlite:///db.sqlite3")
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default="sqlite:///db.sqlite3")
+}
 
 
 
