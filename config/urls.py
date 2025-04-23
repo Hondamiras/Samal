8ba738f20d2912d15216603412583c71b333c40e
Hondamir <157780018+Hondamiras@users.noreply.github.com>
2025-04-23 13:02:52 +0500

    settings

--- config/urls.py
+++ config/urls.py
@@ -14,4 +14,4 @@ urlpatterns = [
 if settings.DEBUG:
     # Раздача медиафайлов
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
